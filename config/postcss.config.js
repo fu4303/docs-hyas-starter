c1bcbd09fc51a9e98b170d78999cd4406699709e
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-07-21 03:17:19 -0400

    Initial commit

---
+++ config/postcss.config.js
@@ -0,0 +1,30 @@
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const whitelister = require('purgecss-whitelister');
+
+module.exports = {
+  plugins: [
+    autoprefixer(),
+    purgecss({
+      content: [
+        './layouts/**/*.html',
+        './content/**/*.md',
+      ],
+      safelist: [
+        'lazyloaded',
+        'table',
+        'thead',
+        'tbody',
+        'tr',
+        'th',
+        'td',
+        'h3',
+        ...whitelister([
+          // './assets/scss/components/_buttons.scss',
+          './assets/scss/components/_code.scss',
+          // './assets/scss/components/_syntax.scss',
+        ]),
+      ],
+    }),
+  ],
+}
